79dcb5f7ff6715388f9ebb1c69605fd9d7546d87
ManGregory <manaenkov.gregory@gmail.com>
2015-03-27 21:51:22 +0200

    refs #1
    добавлена модель для категорий затрат

---
+++ CostsWeb/Models/Category.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace CostsWeb.Models
+{
+    public class CostsContext : DbContext
+    {
+        public CostsContext() : base("DefaultConnection")
+        {            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>()
+                .HasOptional(c => c.Parent)
+                .WithMany()
+                .HasForeignKey(c => c.ParentId);
+        }
+    }
+
+    public class Category
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        
+        [Required]
+        [Display(Name="Название категории")]
+        public string Name { get; set; }
+
+        public int? ParentId { get; set; }
+        public Category Parent { get; set; }
+    }
+}
