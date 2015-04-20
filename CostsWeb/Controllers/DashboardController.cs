2744a453eeb730bf299723844dd4ee38bea766a2
ManGregory <manaenkov.gregory@gmail.com>
2015-04-20 19:42:01 +0300

    refs #8

--- CostsWeb/Controllers/DashboardController.cs
+++ CostsWeb/Controllers/DashboardController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Helpers;
 using System.Web.Mvc;
+using System.Web.UI;
 using CostsWeb.Models;
 using Microsoft.AspNet.Identity;
 
@@ -18,25 +19,40 @@ namespace CostsWeb.Controllers
             get { return User.Identity.GetUserId(); }
         }
 
-        public ActionResult Index()
-        {
+        public ActionResult Index(DateTime? dateFrom, DateTime? dateTo, string chartType = "bar")
+        {           
+            ViewBag.ChartTypes = new SelectList(
+                new[]
+                {
+                    "Area", "Bar", "BoxPlot", "Bubble", "Candlestick", "Column", "Doughnut", "ErrorBar", "FastLine",
+                    "FastPoint", "Funnel", "Kagi", "Line", "Pie", "Point", "PointAndFigure", "Polar", "Pyramid", "Radar",
+                    "Range", "RangeBar", "RangeColumn", "Renko", "Spline", "SplineArea", "SplineRange", "StackedArea",
+                    "StackedArea100", "StackedBar", "StackedBar100", "StackedColumn", "StackedColumn100", "StepLine",
+                    "Stock", "ThreeLineBreak"
+                });
+            TempData["DateFrom"] = dateFrom ?? new DateTime(DateTime.Now.Year, 1, 1);
+            TempData["DateTo"] = dateTo ?? DateTime.Now;
+            TempData["CurrentChartType"] = chartType;
             return View();
         }
 
         public ActionResult Chart()
         {
+            var chartType = TempData["CurrentChartType"] ?? "bar";
+            var dateFrom = TempData["DateFrom"] == null ? new DateTime(DateTime.Now.Year, 1, 1) : (DateTime) TempData["DateFrom"];
+            var dateTo = TempData["DateTo"] == null ? DateTime.Now : (DateTime) TempData["DateTo"];
             var months =
                 System.Globalization.CultureInfo.GetCultureInfoByIetfLanguageTag("ru")
-                    .DateTimeFormat.MonthNames.Take(DateTime.Now.Month).ToArray();
+                    .DateTimeFormat.MonthNames.Take(dateTo.Month).ToArray();
             var sums =
-                db.CostsJournal.Where(c => c.UserId == CurrentUserId && c.Date != null && c.Date.Value.Year == DateTime.Now.Year)
+                db.CostsJournal.Where(c => c.UserId == CurrentUserId && c.Date != null && c.Date.Value >= dateFrom && c.Date.Value <= dateTo)
                     .GroupBy(c => c.Date.Value.Month)
                     .Select(c1 => c1.Sum(c2 => c2.Sum)).ToArray();
-            var myChart = new Chart(width: 600, height: 400)
+            var myChart = new Chart(width: 600, height: 400, theme: ChartTheme.Green)
                 .AddTitle("Chart Title")
                 .AddSeries(
                     name: "Employee",
-                    chartType: "bar",
+                    chartType: chartType.ToString(),
                     xValue: months,
                     yValues: sums);
             return File(myChart.GetBytes("png"), "image/bytes");
